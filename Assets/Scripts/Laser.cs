32229bbbfcb960682923d442607f6416ab97da6e
Seona Bellamy <seona13@gmail.com>
2021-05-27 08:24:22 +1000

    Laser movement
    * The laser now moves up when instantiated.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Laser : MonoBehaviour
+{
+	[SerializeField]
+	private float _speed = 8f;
+
+
+
+	void Start()
+	{
+
+	}
+
+
+	void Update()
+	{
+		transform.Translate(Vector3.up * _speed * Time.deltaTime);
+	}
+}
